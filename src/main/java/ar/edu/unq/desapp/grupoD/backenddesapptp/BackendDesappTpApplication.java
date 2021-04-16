56bbfaacb18d1a7945d4712d6e481205a37d328b
Juan Hualampa <juan.hu.hualampa@gmail.com>
2021-04-15 23:46:44 -0300

    gradle build first commit

---
+++ src/main/java/ar/edu/unq/desapp/grupoD/backenddesapptp/BackendDesappTpApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoD.backenddesapptp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDesappTpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDesappTpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/desapp/grupoD/backenddesapptp/BackendDesappTpApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoD.backenddesapptp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDesappTpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
