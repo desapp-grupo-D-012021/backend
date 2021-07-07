c2d46c99a5334730415fef070466ce752b88a8a4
Juan Hualampa <juan.hu.hualampa@gmail.com>
2021-07-07 15:56:37 -0300

    arch test

--- src/test/java/ArchitectureTests.java
+++ src/test/java/ArchitectureTests.java
@@ -1,10 +1,12 @@
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.library.Architectures;
 import org.junit.Test;
 import org.springframework.stereotype.Service;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
 
 public class ArchitectureTests {
     @Test
@@ -20,6 +22,21 @@ public class ArchitectureTests {
         ArchRule rule = classes().that().haveNameMatching(".*Service").should().resideInAPackage("..service..");
         rule.check(classes);
     }
-    //Controller
+    //Controllers...
+
+    @Test
+    public void CheckLayersAccess() {
+        JavaClasses classes = new ClassFileImporter().importPackages("ar.edu.unq.desapp.grupoD.backenddesapptp");
+        Architectures.LayeredArchitecture architecture = layeredArchitecture()
+                .layer("Controller").definedBy("..webService..")
+                .layer("Model").definedBy("..model..")
+                .layer("Persistence").definedBy("..persistence..")
+                .layer("Security").definedBy("..security..")
+                .layer("Service").definedBy("..service..")
+                .whereLayer("Controller").mayNotBeAccessedByAnyLayer()
+                .whereLayer("Persistence").mayOnlyBeAccessedByLayers("Model", "Security", "Service")
+                .whereLayer("Service").mayOnlyBeAccessedByLayers("Controller", "Security");
+        architecture.check(classes);
+    }
 
 }
